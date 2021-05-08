b898d2505a75abcaef3c5a14393bf7648a774187
Kiranmayi Methuku <kmethuku@protonmail.com>
2021-05-08 09:19:36 -0700

    Added half of table models

--- Server/db.js
+++ Server/db.js
@@ -8,16 +8,142 @@ try {
   console.error('Unable to connect to the database:', error);
 }
 
-// placeholder
+// sequelize will auto-generate createdAt and updatedAt for all tables, so
+// created_on was removed
 const User = sequelize.define('User', {
-  firstName: {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  user_uid: {
     type: DataTypes.STRING,
     allowNull: false
   },
-  lastName: {
+  username: {
     type: DataTypes.STRING,
     allowNull: false
-  }
+  },
+  profile_picture: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  last_login: {
+    type: DataTypes.DATE,
+    allowNull: false
+  },
+  weight: {
+    type: DataTypes.DOUBLE,
+    allowNull: true
+  },
+  height: {
+    type: DataTypes.INTEGER,
+    allowNull: true
+  },
+  type: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  first_name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  last_name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  birthday: { //changed from age to birthday
+    type: DataTypes.DATEONLY,
+    allowNull: true
+  },
 });
 
-module.exports = { User };
+const Invite = sequelize.define('Invite', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  trainer_uid: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  invite_code: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+});
+
+const Workout_Exercise = sequelize.define('Workout_Exercise', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  workout_id: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  exercise_id: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+});
+
+const Workout = sequelize.define('Workout', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  workout_id: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+});
+
+const Exercise = sequelize.define('Exercise', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  trainer_uid: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  type: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  description: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  media: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  muscle_group: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  benefits: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+});
+
+module.exports = { User, Invite, Workout_Exercise, Workout, Exercise };
